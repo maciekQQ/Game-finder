964c1a7a53ebcc49f365046ad7f9dd775e728af6
maciekQQ <maciej.mika@student.wat.edu.pl>
2023-08-11 18:40:13 +0200

    Filter gamesby genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,9 +4,14 @@ import GameCard from "./GameCard";
 
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
+import { Genre } from "../hooks/useGenres";
 
-const GameGrid = () => {
-  const { data, error, isLoading } = useGames();
+interface Props {
+  selectedGenre: Genre | null;
+}
+
+const GameGrid = ({ selectedGenre }: Props) => {
+  const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
@@ -18,8 +23,8 @@ const GameGrid = () => {
       >
         {isLoading &&
           skeletons.map((skeleton) => (
-            <GameCardContainer>
-              <GameCardSkeleton key={skeleton} />
+            <GameCardContainer key={skeleton}>
+              <GameCardSkeleton />
             </GameCardContainer>
           ))}
         {data.map((game) => (
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,4 +1,4 @@
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 
@@ -8,7 +8,7 @@ interface FetchResponse<T> {
   results: T[];
 }
 
-const useData = <T>(endpoint:string) => {
+const useData = <T>(endpoint:string,requestConfig?:AxiosRequestConfig,deps?:any[]) => {
     const [data, setData] = useState<T[]>([]);
     const [error, setError] = useState("");
     const [isLoading, setLoading] = useState(false);
@@ -18,7 +18,7 @@ const useData = <T>(endpoint:string) => {
     
         setLoading(true);
         apiClient
-        .get<FetchResponse<T>>(endpoint, { signal: controller.signal})
+        .get<FetchResponse<T>>(endpoint, { signal: controller.signal,...requestConfig})
         .then((res) => {
           setData(res.data.results);
           setLoading(false);
@@ -30,7 +30,7 @@ const useData = <T>(endpoint:string) => {
         });
 
     return () => controller.abort();
-  }, []);
+  }, deps?[...deps]:[]);
 
   return { data, error, isLoading };
 };
