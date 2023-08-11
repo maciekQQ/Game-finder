91afff68b62ac3ca2ab192b32c70004d712f6089
maciekQQ <maciej.mika@student.wat.edu.pl>
2023-08-11 17:16:51 +0200

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -6,14 +6,14 @@ import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { data, error, isLoading } = useGames();
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding={10}
-        spacing={10}
+        spacing={3}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
