50de2935f4c3a94658b8b45b12457c5f797ac976
maciekQQ <maciej.mika@student.wat.edu.pl>
2023-08-16 21:43:01 +0200

    Sort the games

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
-
+import SortSelector from "./components/SortSelector";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 
@@ -40,12 +40,25 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelector
-          selectedPlatform={gameQuery.platform}
-          onSelectedPlatfrom={(platform) =>
-            setGameQuery({ ...gameQuery, platform })
-          }
-        />
+        <Box paddingLeft={2}>
+          <GameHeading gameQuery={gameQuery} />
+          <Flex marginBottom={5}>
+            <Box marginRight={5}>
+              <PlatformSelector
+                selectedPlatform={gameQuery.platform}
+                onSelectedPlatfrom={(platform) =>
+                  setGameQuery({ ...gameQuery, platform })
+                }
+              />
+            </Box>
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSelectSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+            />
+          </Flex>
+        </Box>
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
     </Grid>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,6 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
+
 
 
 export interface Platform{
@@ -20,9 +20,11 @@ export interface Game {
 const useGames = (
   gameQuery:GameQuery)=> useData<Game>('/games',{
   params:{
-    genres:gameQuery.genre?.id,platforms:gameQuery.platform?.id
-  }},[
-    gameQuery
-  ])
+    genres:gameQuery.genre?.id,
+    platforms:gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
+  }},
+  [gameQuery]
+  )
 
 export default useGames
