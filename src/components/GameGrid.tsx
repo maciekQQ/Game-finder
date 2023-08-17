d5672df7bde1860b755c6d9ce5e1fa7f075a098a
maciekQQ <maciej.mika@student.wat.edu.pl>
2023-08-17 02:49:13 +0200

    Grid changes

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -13,6 +13,9 @@ interface Props {
 const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
+
+  if (error) return <Text>{error}</Text>;
+
   return (
     <>
       {error && <Text>{error}</Text>}
